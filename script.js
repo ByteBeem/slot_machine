93ac9eba0de7a9f9d92af42b5a4af904ce685f63
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2023-12-29 22:36:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,78 +1,88 @@
 const slotSymbols = [
-      ['😀', '😁', '😂', '😃', '😄', '😅', '😆', '😇', '😈', '😉', '😊', '🙂'],
-      ['🍎', '🍏', '🍐', '🍊', '🍋', '🍌', '🍉', '🍇', '🍓', '🍈', '🍒', '🍑'],
-      ['⭐️', '🌟', '✨', '💫', '⚡️', '☄️', '🌠', '🌌', '🌙', '🌕', '🌖', '🌗']
-    ];
+  ['😀', '😁', '😂', '😃', '😄', '😅', '😆', '😇', '😈', '😉', '😊', '🙂'],
+  ['🍎', '🍏', '🍐', '🍊', '🍋', '🍌', '🍉', '🍇', '🍓', '🍈', '🍒', '🍑'],
+  ['⭐️', '🌟', '✨', '💫', '⚡️', '☄️', '🌠', '🌌', '🌙', '🌕', '🌖', '🌗']
+];
 
-    function createSymbolElement(symbol) {
-      const div = document.createElement('div');
-      div.classList.add('symbol');
-      div.textContent = symbol;
-      return div;
+const audio = new Audio('./slot-machine-payout-81725.mp3'); 
+
+function createSymbolElement(symbol) {
+  const div = document.createElement('div');
+  div.classList.add('symbol');
+  div.textContent = symbol;
+  return div;
+}
+
+let spun = false;
+
+function playSlotSound() {
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function spin() {
+  if (spun) {
+    reset();
+  }
+
+  const slots = document.querySelectorAll('.slot');
+  let completedSlots = 0;
+
+  slots.forEach((slot, index) => {
+    const symbols = slot.querySelector('.symbols');
+    const symbolHeight = symbols.querySelector('.symbol')?.clientHeight;
+    const symbolCount = symbols.childElementCount;
+
+    symbols.innerHTML = '';
+
+    symbols.appendChild(createSymbolElement('❓'));
+
+    for (let i = 0; i < 3; i++) {
+      slotSymbols[index].forEach(symbol => {
+        symbols.appendChild(createSymbolElement(symbol));
+      });
     }
 
-    let spun = false;
-    function spin() {
-      if (spun) {
-        reset();
+    const totalDistance = symbolCount * symbolHeight;
+    const randomOffset = -Math.floor(Math.random() * (symbolCount - 1) + 1) * symbolHeight;
+    symbols.style.top = `${randomOffset}px`;
+
+    symbols.addEventListener('transitionend', () => {
+      completedSlots++;
+      if (completedSlots === slots.length) {
+        logDisplayedSymbols();
+        playSlotSound(); 
       }
-      const slots = document.querySelectorAll('.slot');
-      let completedSlots = 0;
+    }, { once: true });
+  });
 
-      slots.forEach((slot, index) => {
-        const symbols = slot.querySelector('.symbols');
-        const symbolHeight = symbols.querySelector('.symbol')?.clientHeight;
-        const symbolCount = symbols.childElementCount;
+  spun = true;
+}
 
-        symbols.innerHTML = '';
+function reset() {
+  const slots = document.querySelectorAll('.slot');
 
-        symbols.appendChild(createSymbolElement('❓'));
+  slots.forEach(slot => {
+    const symbols = slot.querySelector('.symbols');
+    symbols.style.transition = 'none';
+    symbols.style.top = '0';
+    symbols.offsetHeight;
+    symbols.style.transition = '';
+  });
+}
 
-        for (let i = 0; i < 3; i++) {
-          slotSymbols[index].forEach(symbol => {
-            symbols.appendChild(createSymbolElement(symbol));
-          });
-        }
+function logDisplayedSymbols() {
+  const slots = document.querySelectorAll('.slot');
+  const displayedSymbols = [];
 
-        const totalDistance = symbolCount * symbolHeight;
-        const randomOffset = -Math.floor(Math.random() * (symbolCount - 1) + 1) * symbolHeight;
-        symbols.style.top = `${randomOffset}px`;
+  slots.forEach((slot, index) => {
+    const symbols = slot.querySelector('.symbols');
+    const symbolIndex = Math.floor(Math.abs(parseInt(symbols.style.top, 10)) / slot.clientHeight) % slotSymbols[index].length;
+    const displayedSymbol = slotSymbols[index][symbolIndex];
+    displayedSymbols.push(displayedSymbol);
+  });
 
-        symbols.addEventListener('transitionend', () => {
-          completedSlots++;
-          if (completedSlots === slots.length) {
-            logDisplayedSymbols();
-          }
-        }, { once: true });
-      });
+  console.log(displayedSymbols);
+}
 
-      spun = true;
-    }
-
-    function reset() {
-      const slots = document.querySelectorAll('.slot');
-
-      slots.forEach(slot => {
-        const symbols = slot.querySelector('.symbols');
-        symbols.style.transition = 'none';
-        symbols.style.top = '0';
-        symbols.offsetHeight;
-        symbols.style.transition = '';
-      });
-    }
-
-    function logDisplayedSymbols() {
-      const slots = document.querySelectorAll('.slot');
-      const displayedSymbols = [];
-
-      slots.forEach((slot, index) => {
-        const symbols = slot.querySelector('.symbols');
-        const symbolIndex = Math.floor(Math.abs(parseInt(symbols.style.top, 10)) / slot.clientHeight) % slotSymbols[index].length;
-        const displayedSymbol = slotSymbols[index][symbolIndex];
-        displayedSymbols.push(displayedSymbol);
-      });
-
-      console.log(displayedSymbols);
-    }
-
-    spin();
+spin();
